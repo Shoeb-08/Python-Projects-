3747efbbcbad16ac7685158546b53c55b45d6467
Shoeb <hussainyshoeb08@gmail.com>
2025-08-27 16:05:43 +0530

    Project 1 - Generate Unique Name

---
+++ Band_Name_Generator_Project.py
@@ -0,0 +1,4 @@
+print("Welcome!\n")
+username = input("What's the name of the city that you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name is "+ username + " " + pet)
